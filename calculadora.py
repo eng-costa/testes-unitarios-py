ba010faee1b7e1d006990ad85e10e1656e527722
eng-costa <computer.lucas2@gmail.com>
2024-11-13 17:35:22 -0300

    Atualização dos arquivos com o de sala de aula

--- calculadora.py
+++ calculadora.py
@@ -1,21 +1,30 @@
-# 1) Criar a cllasse chamada "calculadora"
-class Calculadora:
+# 1) Criar a classe
+# chamada "Calculadora"
 
-    # 2) Criar o método chamado "soma()"
-    def soma(self, n1,  n2):
+# correspondente do "this"
+# em Python: self 
+
+class Calculadora:
+    
+    # 2) Criar o método
+    # chamado "soma()"
+    def soma(self, n1, n2):
         return n1 + n2
-     
-    # 3) Criar o método chamado "subtracao()"
-    def subtracao(self, n1,  n2):
+    
+    # 3) Criar o método
+    # chamado "subtracao()"
+    def subtracao(self, n1, n2):
         return n1 - n2
     
-    # 4) Criar o método chamado "multiplicacao()"
-    def multiplicacao(self, n1,  n2):
+    # 4) Criar o método
+    # chamado "multiplicacao()"
+    def multiplicacao(self, n1, n2):
         return n1 * n2
     
-    # 5) Criar o método chamado "divisao()"
-    def divisao(self, n1,  n2):
-        if n2 == 0:
-            return "Divisao invalida"
+    # 4) Criar o método
+    # chamado "divisao()"
+    def divisao(self, n1, n2):
+        if n2 == 0:            
+            return "Divisão inválida"
         else:
             return n1 / n2
